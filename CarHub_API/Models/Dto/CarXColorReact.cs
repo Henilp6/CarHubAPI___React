cca79922ce7cd49e0c0b5aa39cf0784f142a3b2a
Henil Patel <patelhenilk2001@gmail.com>
2023-12-13 13:28:28 +0530

    Add project files.

---
+++ CarHub_API/Models/Dto/CarXColorReact.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+
+namespace CarHub_API.Models.Dto
+{
+    public class CarXColorReact
+    {
+        public int CarId { get; set; }
+        public List<String> SelectedColorIds { get; set; }
+    }
+}
